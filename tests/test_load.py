ed091ee3d528ba945f7076d3d4d15deaaf9fd42f
Ben <ben.daccache@gmail.com>
2018-04-15 11:00:32 +0200

    change load settings

--- tests/test_load.py
+++ tests/test_load.py
@@ -5,8 +5,8 @@ import httplib
 from my_utils import *
 import threading
 
-NB_THREADS = 100
-NB_REPEAT_PER_THREAD = 100
+NB_THREADS = 10
+NB_REPEAT_PER_THREAD = 10
 
 class TestChargeHttpServer(unittest.TestCase):
 
